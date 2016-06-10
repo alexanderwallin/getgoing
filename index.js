b6ed8217f2a0abc580e24f55db8a2d4224f78e95
Alexander Wallin <office@alexanderwallin.com>
2016-06-10 11:54:54 +0100

    Supports specifying a file to be copied

--- index.js
+++ index.js
@@ -3,16 +3,29 @@
 const path = require('path')
 const fs = require('fs')
 const glob = require('glob')
+const yargs = require('yargs')
 
-console.log('getgoing')
+const args = yargs.argv
 
-glob(`${__dirname}/templates/{.*,*.*}`, (err, files) => {
-  console.log(err)
-  console.log(files)
+const fileGlob = args._.length ? args._[0] : '{.*,*.*}'
+const globStr = `${__dirname}/templates/${fileGlob}`
+
+console.log('\ngetgoing')
+
+glob(globStr, (err, files) => {
+  if (err) {
+    console.log('Error occured:')
+    console.log(err)
+    process.exit(1)
+  }
+
+  console.log('\nCopying:')
 
   files.forEach(file => {
     const filename = path.basename(file)
     const destination = path.join(process.cwd(), filename)
+
+    console.log(' -', filename)
 
     fs.createReadStream(file)
       .pipe(fs.createWriteStream(destination))
